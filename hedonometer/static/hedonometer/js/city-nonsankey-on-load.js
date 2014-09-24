927691b1373afcb6902de1f87e3ee27339e3e80c
Andy Reagan <github@andyreagan.com>
2014-09-24 18:15:41 -0400

    added the nonsankey for chris

---
+++ hedonometer/static/hedonometer/js/city-nonsankey-on-load.js
@@ -0,0 +1,202 @@
+hedotools.sankeyoncall = function() { 
+    var test = function(i,data) {
+	console.log("set on call");
+    }
+    var opublic = { test: test, };
+    return opublic;
+}();
+
+var intStr = ["one","two"];
+var timeFrames= ["2011","2012","2013","2014"];
+
+var reftimeselencoder = d3.urllib.encoder().varname("ref");
+var reftimeseldecoder = d3.urllib.decoder().varname("ref").varresult("2011");
+d3.select(".reftimelabel").html("All tweets from "+reftimeseldecoder().cached+' <span class="caret"></span>');
+d3.select(".reftimelabelbottom").html("All tweets from "+reftimeseldecoder().cached+' <span class="caret"></span>');
+
+var comptimeselencoder = d3.urllib.encoder().varname("comp");
+var comptimeseldecoder = d3.urllib.decoder().varname("comp").varresult("2012");
+d3.select(".comptimelabel").html("All tweets from "+comptimeseldecoder().cached+' <span class="caret"></span>');
+d3.select(".comptimelabelbottom").html("All tweets from "+comptimeseldecoder().cached+' <span class="caret"></span>');
+
+// to store the "select as reference city" selection
+var refcity = "";
+
+// share-able shifts!
+var refshifttimeencoder = d3.urllib.encoder().varname("reftime");
+var refshifttimedecoder = d3.urllib.decoder().varname("reftime").varresult("");
+var refshiftcityencoder = d3.urllib.encoder().varname("refcity");
+var refshiftcitydecoder = d3.urllib.decoder().varname("refcity").varresult("");
+var compshifttimeencoder = d3.urllib.encoder().varname("comptime");
+var compshifttimedecoder = d3.urllib.decoder().varname("comptime").varresult("");
+var compshiftcityencoder = d3.urllib.encoder().varname("compcity");
+var compshiftcitydecoder = d3.urllib.decoder().varname("compcity").varresult("");
+
+// get rid of them when the modal closes
+$('#myModal').on('hidden.bs.modal', function (e) {
+    refshifttimeencoder.varval("none");
+    refshifttimeencoder.destroy();
+    refshiftcityencoder.varval("none");
+    refshiftcityencoder.destroy();
+    compshifttimeencoder.varval("none");
+    compshifttimeencoder.destroy();
+    compshiftcityencoder.varval("none");
+    compshiftcityencoder.destroy();
+});
+
+function initializePlot() {
+    timeDrop();
+    loadCsv();
+}
+
+function timeDrop() {
+    d3.select("#refSelect").selectAll("a")
+        .on("click", function(d,i) {
+	    key = i;
+	    d3.select(".reftimelabel").html("All tweets from "+timeFrames[key]+' <span class="caret"></span>');
+	    d3.select(".reftimelabelbottom").html("All tweets from "+timeFrames[key]+' <span class="caret"></span>');
+	    reftimeselencoder.varval(timeFrames[key]);
+	    d3.text("http://hedonometer.org/data/cities/cityList_"+(reftimeseldecoder().cached)+"_mutualHapps.csv", function(text) {
+		ref = text.split("\n").slice(0,304);
+		hedotools.sankey.setdata(ref,comp,cities).replot();
+	    });
+	});
+    d3.select("#compSelect").selectAll("a")
+        .on("click", function(d,i) {
+            // key = this.selectedIndex;
+	    key = i;
+	    d3.select(".comptimelabel").html("All tweets from "+timeFrames[key]+' <span class="caret"></span>');
+	    d3.select(".comptimelabelbottom").html("All tweets from "+timeFrames[key]+' <span class="caret"></span>');
+	    comptimeselencoder.varval(timeFrames[key]);
+	    d3.text("http://hedonometer.org/data/cities/cityList_"+(comptimeseldecoder().cached)+"_mutualHapps.csv", function(text) {
+		comp = text.split("\n").slice(0,304);
+		hedotools.sankey.setdata(ref,comp,cities).replot();
+	    });
+	});
+    d3.select("#refSelectBottom").selectAll("a")
+        .on("click", function(d,i) {
+	    key = i;
+	    d3.select(".reftimelabel").html("All tweets from "+timeFrames[key]+' <span class="caret"></span>');
+	    d3.select(".reftimelabelbottom").html("All tweets from "+timeFrames[key]+' <span class="caret"></span>');
+	    reftimeselencoder.varval(timeFrames[key]);
+	    d3.text("http://hedonometer.org/data/cities/cityList_"+(reftimeseldecoder().cached)+"_mutualHapps.csv", function(text) {
+		ref = text.split("\n").slice(0,304);
+		hedotools.sankey.setdata(ref,comp,cities).replot();
+	    });
+	});
+    d3.select("#compSelectBottom").selectAll("a")
+        .on("click", function(d,i) {
+            // key = this.selectedIndex;
+	    key = i;
+	    d3.select(".comptimelabel").html("All tweets from "+timeFrames[key]+' <span class="caret"></span>');
+	    d3.select(".comptimelabelbottom").html("All tweets from "+timeFrames[key]+' <span class="caret"></span>');
+	    comptimeselencoder.varval(timeFrames[key]);
+	    d3.text("http://hedonometer.org/data/cities/cityList_"+(comptimeseldecoder().cached)+"_mutualHapps.csv", function(text) {
+		comp = text.split("\n").slice(0,304);
+		hedotools.sankey.setdata(ref,comp,cities).replot();
+	    });
+	});
+}
+
+function loadCsv() {
+    var csvLoadsRemaining = 3;
+    d3.text("http://hedonometer.org/data/cities/cityList_"+(reftimeseldecoder().cached)+"_mutualHapps.csv", function(text) {
+	ref = text.split("\n").slice(0,304);
+	if (!--csvLoadsRemaining) initializePlotPlot();
+    });
+    d3.text("http://hedonometer.org/data/cities/cityList_"+(comptimeseldecoder().cached)+"_mutualHapps.csv", function(text) {
+	comp = text.split("\n").slice(0,304);;
+	if (!--csvLoadsRemaining) initializePlotPlot();
+    });
+    d3.text("http://hedonometer.org/data/cities/mutualCities.csv", function(text) {
+	cities = text.split("\n").slice(0,304);;
+	if (!--csvLoadsRemaining) initializePlotPlot();
+    });
+};
+
+function initializePlotPlot() {
+
+    classColor.domain([cities.length,1]);
+    
+    hedotools.sankey.setSideWidth([0,0]).setfigure(d3.select("#sankeyChart")).setdata(ref,comp,cities).setTipOn().plot().setTitles(['2011','2012']);
+    
+    // set the figure initially
+    hedotools.shifter.setfigure(d3.select('#shift')).setHeight(400);
+    var ignoreWords = ["severe","flood","warning","earthquake","hi","me","new","humidity","pressure","burns","emergency","in","la","ms","mt","oh","ok","or","pa","ma","hawaii","virginia","grand","springs","falls","battle","old","miami"];
+    hedotools.shifter.ignore(ignoreWords);
+
+    var popupshiftct = 4;
+    // check if going to pop up an shift
+
+    console.log("popping up shift");
+    var refyear = refshifttimedecoder().cached;
+    var refname = refshiftcitydecoder().cached;
+    var compyear = compshifttimedecoder().cached;
+    var compname = compshiftcitydecoder().cached;
+    // write a function to call on the load
+    drawShift = function() {
+	hedotools.shifter._refF(refF);
+	hedotools.shifter._compF(compF);
+	hedotools.shifter.stop();
+	hedotools.shifter.shifter();
+	hedotools.shifter.setText("Why "+compname+" in "+compyear+" is "+( ( hedotools.shifter._compH() > hedotools.shifter._refH() ) ? "happier" : "less happy" )+" than "+refname+" in "+refyear+":").plot();
+	$('#myModal').modal('show');
+    }
+    // load both of the files
+    var csvLoadsRemaining = 2;
+    var reffile = "http://hedonometer.org/data/cities/word-vectors/"+refyear+"/"+refname+".csv";
+    if (parseInt(refyear) < 2014) reffile+=".new"
+    var compfile = "http://hedonometer.org/data/cities/word-vectors/"+compyear+"/"+compname+".csv";
+    if (parseInt(compyear) < 2014) compfile+=".new"
+    console.log(reffile);
+    console.log(compfile);
+    var refF;
+    var compF;
+    if ( refshifttimedecoder().cached.length > 0) {
+	d3.text(reffile,function(text) {
+	    refF = text.split(",");
+	    console.log(refF);
+	    if (!--popupshiftct) drawShift();
+	});
+	d3.text(compfile,function(text) {
+	    compF = text.split(",");
+	    console.log(compF);
+	    if (!--popupshiftct) drawShift();
+	});
+    }
+
+    // set some data
+    var scoresFile = "http://hedonometer.org/data/labMT/labMTscores-english.csv";
+    var wordsFile = "http://hedonometer.org/data/labMT/labMTwords-english.csv";
+    d3.text(scoresFile, function(text) {
+	var tmp = text.split("\n");
+	console.log("loaded words");
+	//console.log(tmp[tmp.length-1]);
+	var lens = tmp.map(function(d) { return parseFloat(d); });
+	var len = lens.length - 1;
+	while (!lens[len]) {
+	    //console.log("in while loop");
+	    lens = lens.slice(0, len);
+	    len--;
+	}
+	hedotools.shifter._lens(lens);
+	if (!--popupshiftct) drawShift();
+    });
+    d3.text(wordsFile, function(text) {
+	var tmp = text.split("\n");
+	var words = tmp;
+	var len = words.length - 1;
+	while (!words[len]) {
+	    //console.log("in while loop");
+	    words = words.slice(0, len);
+	    len--;
+	}
+	hedotools.shifter._words(words);
+	if (!--popupshiftct) drawShift();
+    });
+};
+
+initializePlot();
+
+
+
--- hedonometer/static/hedonometer/js/hedotools.nonsankey.js
+++ hedonometer/static/hedonometer/js/hedotools.nonsankey.js
@@ -112,7 +112,7 @@ hedotools.sankey = function() {
     // make the plot
     var plot = function() {
 	margin = {top: 0, right: 0, bottom: 0, left: 0};
-	axeslabelmargin = {top: 0, right: 90+extraSideWidth[0], bottom: 0, left: 90+extraSideWidth[1]};
+	axeslabelmargin = {top: 0, right: 0+extraSideWidth[0], bottom: 0, left: 0+extraSideWidth[1]};
 	figwidth = parseInt(figure.style('width')) - margin.left - margin.right;
 	if (figwidth<minwidth) {
 	    console.log("width is too small...");
@@ -163,7 +163,7 @@ hedotools.sankey = function() {
 	// linear scale function
 	y =  d3.scale.linear()
 	    .domain([newlist.length,1])
-	    .range([height-20, 5]); 
+	    .range([height-20, 0]); 
 
 	// create the axes themselves
 	axes = canvas.append("g")
@@ -185,90 +185,19 @@ hedotools.sankey = function() {
 	    .append("text")
 	    .attr("class", function(d,i) { return d.name+" statetext"; })
 	    .attr("x",20)
-	    .style("text-anchor", "end")
-	    .attr("y",function(d,i) { return y(d.oldindex+1)+11; } )
-            .text(function(d,i) { return (d.oldindex+1)+". "+d.name; });
-
-	newstateselection = axes.selectAll("text.statetext.new")
-	    .data(data)
-	    .enter()
-	    .append("text")
-	    .attr("class", function(d,i) { return d.name+" statetext"; })
-	    .attr("x",width-20)
 	    .style("text-anchor", "start")
-	    .attr("y",function(d,i) { return y(d.newindex+1)+11; } )
-            .text(function(d,i) { return (d.newindex+1)+". "+d.name; });
-
-	// create an instance of the sankey to make paths
-	var sankey = d3.sankey();
-	path = sankey.link();
-
-	// create the sankey data thingy
-	sankeydata = Array(oldlist.length);
-	for (var i=0; i<data.length; i++) {
-	    sankeydata[i] = {
-		"source": {
-		    "x": 20,
-		    "dx": 2,
-		    "y": y(data[i].oldindex+1)-8, 
-		},
-		"target": {
-		    "x": width-22,
-		    "dx": 2,
-		    "y": y(data[i].newindex+1)-8,
-		},
-		"name": data[i].name,
-		"oldhapps": data[i].oldhapps,
-		"newhapps": data[i].newhapps,
-		"oldindex": data[i].oldindex,
-		"newindex": data[i].newindex,
-		"sy": 10,
-		"ty": 10,
-		"dy": 10,
-	    };
-	}
-
-	pathwidth = d3.scale.linear()
-	    .domain(d3.extent(data.map(function(d) { return Math.abs(d.change); })))
-	    .range([2,13]);
-
-	pathselection = axes.selectAll("path.sankey").data(sankeydata)
-	    .enter()
-	    .append("path")
-            .attr({ "d": path,
-		    "fill": "none",
-		    "class": function(d,i) { return "r"+classColor(data[i].oldindex)+"-8"; },
-		    "stroke-width": function(d,i) { return pathwidth(Math.abs(data[i].change)); } })
+	    .attr("y",function(d,i) { return y(d.oldindex+1)+11; } )
+            .text(function(d,i) { return (d.oldindex+1)+". "+d.name; })
 	    .on("mouseover", function(d,i) { 
-		// console.log(i);
-		// console.log(data[i]);
-		var rectSelection = d3.select(this)
-		    .style({'opacity':'0.7',
-			    // 'stroke-width':'1.0',
-			   });
-
-		var thispath = this;
-
-		hedotools.sankeyoncall.test(i,data);
-
-		if (useTip) {
-
-		    // var bbox = this.getBBox(); 
-		    // var x = Math.floor(bbox.x + bbox.width/2.0); 
-		    // var y = Math.floor(bbox.y + bbox.height/2.0);
-
 		    var hoverboxheight = 90;
 		    var hoverboxwidth = 200;
 		    var hoverboxyoffset = 0;
 		    var hoverboxxoffset = 0;
 
-		    var x = d3.mouse(thispath)[0];
-		    var y = d3.mouse(thispath)[1];
+		    var x = d3.mouse(this)[0];
+		    var y = d3.mouse(this)[1];
 		    
 		    if ((y+270)>height) { y-=(y+270-height); }
-
-		    // tip.show;
-		    // console.log(d);
 
 		    hovergroup.style({
 			"position": "absolute",
@@ -315,10 +244,6 @@ hedotools.sankey = function() {
 			}
 			// load both of the files
 			var csvLoadsRemaining = 2;
-			// var reffile = "http://hedonometer.org/data/cities/word-vectors/"+reftimeseldecoder().cached+"/"+d.name+".csv";
-			// if (parseInt(reftimeseldecoder().cached) < 2014) reffile+=".new"
-			// var compfile = "http://hedonometer.org/data/cities/word-vectors/"+comptimeseldecoder().cached+"/"+d.name+".csv";
-			// if (parseInt(comptimeseldecoder().cached) < 2014) compfile+=".new"
 			var reffile = "http://hedonometer.org/data/cities/word-vectors/"+refyear+"/"+refname+".csv";
 			if (parseInt(refyear) < 2014) reffile+=".new"
 			var compfile = "http://hedonometer.org/data/cities/word-vectors/"+compyear+"/"+compname+".csv";
@@ -407,23 +332,179 @@ hedotools.sankey = function() {
 				popupshift(reftimeseldecoder().cached,refcity,reftimeseldecoder().cached,d.name);
 			    });
 		    }
-		}
-		
+
 		clearTimeout(popuptimer);
 
 		popuptimer = setTimeout(hidehover,3000);
 	    })
 	    .on("mouseout", function(d,i) { 
-		if (useTip) {
-		    // hovergroup.style({
-		    // 	"visibility": "hidden",
-		    // });
-		    clearTimeout(popuptimer);
+		clearTimeout(popuptimer);
 
-		    popuptimer = setTimeout(hidehover,3000);
-		}
-		var rectSelection = d3.select(this)
-		    .style({ 'opacity':'1.0', }) 
+		popuptimer = setTimeout(hidehover,3000);
+	    });
+
+	newstateselection = axes.selectAll("text.statetext.new")
+	    .data(data)
+	    .enter()
+	    .append("text")
+	    .attr("class", function(d,i) { return d.name+" statetext"; })
+	    .attr("x",300)
+	    .style("text-anchor", "start")
+	    .attr("y",function(d,i) { return y(d.newindex+1)+11; } )
+            .text(function(d,i) { return (d.newindex+1)+". "+d.name; })
+	    .on("mouseover", function(d,i) { 
+		    var hoverboxheight = 90;
+		    var hoverboxwidth = 200;
+		    var hoverboxyoffset = 0;
+		    var hoverboxxoffset = 0;
+
+		    var x = d3.mouse(this)[0];
+		    var y = d3.mouse(this)[1];
+		    
+		    if ((y+270)>height) { y-=(y+270-height); }
+
+		    hovergroup.style({
+			"position": "absolute",
+			"top": y+"px",
+			"left": x+"px",
+			"visibility": "visible",
+		    });
+
+		    hovergroup.selectAll("p,h3,button,br").remove();
+
+		    hovergroup.append("h3")
+			.attr("class","cityname")
+			.text(d.name);
+
+		    hovergroup.append("p")
+			.attr("class","refhapps")
+		    	.text(reftimeseldecoder().cached+" Happiness: "+parseFloat(d.oldhapps).toFixed(2));
+
+		    hovergroup.append("p")
+			.attr("class","refrank")
+		    	.text(reftimeseldecoder().cached+" Rank: "+(d.oldindex+1));
+
+		    hovergroup.append("p")
+			.attr("class","comphapps")
+		    	.text(comptimeseldecoder().cached+" Happiness: "+parseFloat(d.newhapps).toFixed(2));
+
+		    hovergroup.append("p")
+			.attr("class","comprank")
+		    	.text(comptimeseldecoder().cached+" Rank: "+(d.newindex+1));
+
+		    var popupshift = function(refyear,refname,compyear,compname) {
+			refshifttimeencoder.varval(refyear);
+			refshiftcityencoder.varval(refname);
+			compshifttimeencoder.varval(compyear);
+			compshiftcityencoder.varval(compname);
+			// write a function to call on the load
+			drawShift = function() {
+			    hedotools.shifter._refF(refF);
+			    hedotools.shifter._compF(compF);
+			    hedotools.shifter.stop();
+			    hedotools.shifter.shifter();
+			    hedotools.shifter.setText("Why "+compname+" in "+compyear+" is "+( ( hedotools.shifter._compH() > hedotools.shifter._refH() ) ? "happier" : "less happy" )+" than "+refname+" in "+refyear+":").plot();
+			    $('#myModal').modal('show');
+			}
+			// load both of the files
+			var csvLoadsRemaining = 2;
+			var reffile = "http://hedonometer.org/data/cities/word-vectors/"+refyear+"/"+refname+".csv";
+			if (parseInt(refyear) < 2014) reffile+=".new"
+			var compfile = "http://hedonometer.org/data/cities/word-vectors/"+compyear+"/"+compname+".csv";
+			if (parseInt(compyear) < 2014) compfile+=".new"
+			console.log(reffile);
+			console.log(compfile);
+			var refF;
+			var compF;
+			d3.text(reffile,function(text) {
+			    refF = text.split(",");
+			    console.log(refF);
+			    if (!--csvLoadsRemaining) drawShift();
+			});
+			d3.text(compfile,function(text) {
+			    compF = text.split(",");
+			    console.log(compF);
+			    if (!--csvLoadsRemaining) drawShift();
+			});
+		    }
+
+		    hovergroup.append("button")
+			.attr("class","btn btn-sm btn-primary")
+		    	.text("Shift city vs previous year")
+			.on("click", function() {
+			    console.log(d);
+			    console.log(i);
+			    popupshift(reftimeseldecoder().cached,d.name,comptimeseldecoder().cached,d.name);
+			});
+
+		    hovergroup.append("br");
+		    hovergroup.append("br");
+
+		    hovergroup.append("button")
+			.attr("class","btn btn-sm btn-primary")
+		    	.text("Shift city in "+reftimeseldecoder().cached+" vs sum "+reftimeseldecoder().cached)
+			.on("click", function() {
+			    console.log(d);
+			    console.log(i);
+			    popupshift(reftimeseldecoder().cached,"US",reftimeseldecoder().cached,d.name);
+			});
+
+		    hovergroup.append("br");
+		    hovergroup.append("br");
+
+		    hovergroup.append("button")
+			.attr("class","btn btn-sm btn-primary")
+		    	.text("Shift city in "+comptimeseldecoder().cached+" vs sum "+comptimeseldecoder().cached)
+			.on("click", function() {
+			    console.log(d);
+			    console.log(i);
+			    popupshift(comptimeseldecoder().cached,"US",comptimeseldecoder().cached,d.name);
+			});
+
+		    hovergroup.append("br");
+		    hovergroup.append("br");
+
+
+		    hovergroup.append("button")
+			.attr("class","btn btn-xs btn-primary")
+		    	.text("Select as reference for city-city comparison")
+			.on("click", function() {
+			    console.log(d);
+			    console.log(i);
+			    refcity = d.name;
+			});
+
+		    if (refcity.length > 0) {
+			hovergroup.append("br");
+			hovergroup.append("br");
+			hovergroup.append("button")
+			    .attr("class","btn btn-xs btn-primary")
+		    	    .text("Compare against "+refcity+" in "+comptimeseldecoder().cached)
+			    .on("click", function() {
+				console.log(d);
+				console.log(i);
+				popupshift(comptimeseldecoder().cached,refcity,comptimeseldecoder().cached,d.name);
+			    });
+			hovergroup.append("br");
+			hovergroup.append("br");
+			hovergroup.append("button")
+			    .attr("class","btn btn-xs btn-primary")
+		    	    .text("Compare against "+refcity+" in "+reftimeseldecoder().cached)
+			    .on("click", function() {
+				console.log(d);
+				console.log(i);
+				popupshift(reftimeseldecoder().cached,refcity,reftimeseldecoder().cached,d.name);
+			    });
+		    }
+
+		clearTimeout(popuptimer);
+
+		popuptimer = setTimeout(hidehover,3000);
+	    })
+	    .on("mouseout", function(d,i) { 
+		clearTimeout(popuptimer);
+
+		popuptimer = setTimeout(hidehover,3000);
 	    });
 
 	return hedotools.sankey;
@@ -447,43 +528,6 @@ hedotools.sankey = function() {
 	    .duration(3000)
             .text(function(d,i) { return (d.newindex+1)+". "+d.name; })
     	    .attr("y",function(d,i) { return y(d.newindex+1)+11; } );
-
-	// create the sankey data thingy
-	for (var i=0; i<data.length; i++) {
-	    sankeydata[i] = {
-		"source": {
-		    "x": 20,
-		    "dx": 2,
-		    "y": y(data[i].oldindex+1)-8, 
-		},
-		"target": {
-		    "x": width-22,
-		    "dx": 2,
-		    "y": y(data[i].newindex+1)-8,
-		},
-		"name": data[i].name,
-		"oldhapps": data[i].oldhapps,
-		"newhapps": data[i].newhapps,
-		"oldindex": data[i].oldindex,
-		"newindex": data[i].newindex,
-		"sy": 10,
-		"ty": 10,
-		"dy": 10,
-	    };
-	}
-
-	// update the width function
-	pathwidth.domain(d3.extent(data.map(function(d) { return Math.abs(d.change); })));
-
-	pathselection.data(sankeydata)
-	    .transition()
-	    .duration(3000)
-            .attr({ "d": path,
-		    // don't update this
-		    // because the transition is applied by the css at the end
-		    // and it messes up the whole effect
-		    // "class": function(d,i) { return "r"+classColor(data[i].oldindex)+"-8"; },
-		    "stroke-width": function(d,i) { return pathwidth(Math.abs(data[i].change)); } });
 
 	return hedotools.sankey;
     };
