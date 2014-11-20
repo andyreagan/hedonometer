5a27e176f637ab184e7bc7f08bbc958560066a03
Andy Reagan <github@andyreagan.com>
2014-11-20 12:25:21 -0500

    fixing live bugs

--- hedonometer/static/hedonometer/js/drawBookTimeseries-harry.js
+++ hedonometer/static/hedonometer/js/drawBookTimeseries-harry.js
@@ -161,62 +161,6 @@ hedotools.booktimeseries = function() {
 		    "stroke-width": "1.5px",
 		});
 
-	    // var tick = function() {
-	    // 	console.log("ticking...");
-	    // 	// bubbles.attr()
-	    // 	// bubblelines.attr()
-	    // }
-
-	    // var longernodes = nodes.concat(data.map(function(d,i) {
-	    // 	return {
-	    // 	    "x":x(i),
-	    // 	    "y":y(d),
-	    // 	}
-	    // }));
-
-	    // var testnodes = [
-	    // 	{"x": 469, "y": 410},
-	    // 	{"x": 493, "y": 364},
-	    // 	{"x": 442, "y": 365},
-	    // 	{"x": 467, "y": 314},
-	    // 	{"x": 477, "y": 248},
-	    // 	{"x": 425, "y": 207},
-	    // 	{"x": 402, "y": 155},
-	    // 	{"x": 369, "y": 196},
-	    // 	{"x": 350, "y": 148},
-	    // 	{"x": 539, "y": 222},
-	    // 	{"x": 594, "y": 235},
-	    // 	{"x": 582, "y": 185},
-	    // 	{"x": 633, "y": 200}
-	    // ];
-
-	    // var testlinks = [
-	    // 	{"source":  0, "target":  1},
-	    // 	{"source":  1, "target":  2},
-	    // 	{"source":  2, "target":  0},
-	    // 	{"source":  1, "target":  3},
-	    // 	{"source":  3, "target":  2},
-	    // 	{"source":  3, "target":  4},
-	    // 	{"source":  4, "target":  5},
-	    // 	{"source":  5, "target":  6},
-	    // 	{"source":  5, "target":  7},
-	    // 	{"source":  6, "target":  7},
-	    // 	{"source":  6, "target":  8},
-	    // 	{"source":  7, "target":  8},
-	    // 	{"source":  9, "target":  4},
-	    // 	{"source":  9, "target": 11},
-	    // 	{"source":  9, "target": 10},
-	    // 	{"source": 10, "target": 11},
-	    // 	{"source": 11, "target": 12},
-	    // 	{"source": 12, "target": 10}
-	    // ];
-
-	    // console.log(longernodes);
-
-	    // force.nodes(testnodes)
-	    // 	.links(testlinks)
-	    // 	.on("tick",tick);
-
 	    var timeseries = data.map(function(d,i) { return [x(i),y(d)]; });
 	    console.log(timeseries);
 	    console.log(nodes);
@@ -424,6 +368,8 @@ hedotools.booktimeseries = function() {
 	}
 	console.log(minhapps);
 
+	console.log(y);
+	console.log(data);
 	var mincircles  = axes.selectAll("circle.mincircle")
 	    .data(minhapps)
 	    .enter()
