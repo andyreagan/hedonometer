48d626099b34e932ffa3aac23d31301c41eaf890
Andy Reagan <github@andyreagan.com>
2014-12-17 13:10:00 -0500

    catch blank entries, really

--- hedonometer/static/hedonometer/js/compare-on-load.js
+++ hedonometer/static/hedonometer/js/compare-on-load.js
@@ -19,13 +19,21 @@ d3.select("#makewordshift")
 function validateForm() {
     var ref = document.forms["wordshifterator"]["refText"].value;
     var comp = document.forms["wordshifterator"]["compText"].value;
+    if (ref === "") {
+        alert("Enter reference text first");
+        return false;
+    }
+    if (comp === "") {
+        alert("Enter comparison text first");
+        return false;
+    }
     var refwords = ref.match(/[\w\@\#\'\&\]\*\-\/\[\=\;]+/gi);
     var compwords = comp.match(/[\w\@\#\'\&\]\*\-\/\[\=\;]+/gi);
-    if (refwords.length < 1000 || ref === "") {
+    if (refwords.length < 1000) {
         alert("Too few words in reference text (found "+refwords.length+")");
         return false;
     }
-    if (compwords.length < 1000 || comp === "") {
+    if (compwords.length < 1000) {
         alert("Too few words in comparison text (found "+compwords.length+")");
         return false;
     }
