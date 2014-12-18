4020a8e58db35b8b479d515fc157619e7192baed
Andy Reagan <github@andyreagan.com>
2014-12-18 13:12:46 -0500

    it was

--- hedonometer/models.py
+++ hedonometer/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-from django.contrib.auth import get_user_model
-User = get_user_model()
-
 # Create your models here.
 class Happs(models.Model):
     date = models.DateTimeField()
