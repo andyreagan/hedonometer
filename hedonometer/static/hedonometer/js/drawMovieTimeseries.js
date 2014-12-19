fea33113b05de5aa0d9b056ae65055001d98e2a5
Andy Reagan <github@andyreagan.com>
2014-12-19 16:07:28 -0500

    loading the right file

--- hedonometer/static/hedonometer/js/drawMovieTimeseries.js
+++ hedonometer/static/hedonometer/js/drawMovieTimeseries.js
@@ -155,7 +155,7 @@ hedotools.booktimeseries = function() {
 	    });
 
 	    var fheight;
-	    var moviefile2 = "http://hedonometer.org/data/moviedata/rawer/"+movieref+".html.end.beg.clean2";
+	    var moviefile2 = "http://hedonometer.org/data/moviedata/rawer/"+movieref+".html.end.beg";
 	    d3.text(moviefile2, function (text) {
 		// use this to test the total words in this different file
 		// lwords = text.match(/[\w\@\#\'\&\]\*\-\/\[\=\;]+/gi); 
