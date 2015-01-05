02c9a0920e1f65f4de88601f22750092de53c1df
Andy Reagan <github@andyreagan.com>
2015-01-04 21:25:43 -0500

    making all the data

--- hedonometer/static/hedonometer/teledata/fakedata.py
+++ hedonometer/static/hedonometer/teledata/fakedata.py
@@ -1,17 +1,104 @@
 import random
 
-f = open("teledata-50y-maxT.csv","w")
+for y in ["1","10","20","50"]:
+    f = open("teledata-"+y+"y-maxT.csv","w")
+    
+    numy = 100
+    ystart = 1865
+    years = [ystart+i for i in range(numy)]
+    
+    numcities = 1218
+    
+    f.write(",".join(map(str,years)))
+    for i in range(numcities):
+        temps = [random.random()*50+50 for i in range(numy)]
+        f.write("\n")
+        f.write(",".join(map(str,temps)))
+    
+    f.close()
 
-numy = 100
-ystart = 1865
-years = [ystart+i for i in range(numy)]
+for y in ["1","10","20","50"]:
+    f = open("teledata-"+y+"y-minT.csv","w")
+    
+    numy = 100
+    ystart = 1865
+    years = [ystart+i for i in range(numy)]
+    
+    numcities = 1218
+    
+    f.write(",".join(map(str,years)))
+    for i in range(numcities):
+        temps = [random.random()*50 for i in range(numy)]
+        f.write("\n")
+        f.write(",".join(map(str,temps)))
+    
+    f.close()
 
-numcities = 1218
 
-f.write(",".join(map(str,years)))
-for i in range(numcities):
-    temps = [random.random()*50+50 for i in range(numy)]
-    f.write("\n")
-    f.write(",".join(map(str,temps)))
+for y in ["1","10","20","50"]:
+    f = open("teledata-"+y+"y-summer_day.csv","w")
+    
+    numy = 100
+    ystart = 1865
+    years = [ystart+i for i in range(numy)]
+    
+    numcities = 1218
+    
+    f.write(",".join(map(str,years)))
+    for i in range(numcities):
+        temps = [random.randint(1,200) for i in range(numy)]
+        f.write("\n")
+        f.write(",".join(map(str,temps)))
+    
+    f.close()
 
-f.close()
+for y in ["1","10","20","50"]:
+    f = open("teledata-"+y+"y-summer_extent.csv","w")
+    
+    numy = 100
+    ystart = 1865
+    years = [ystart+i for i in range(numy)]
+    
+    numcities = 1218
+    
+    f.write(",".join(map(str,years)))
+    for i in range(numcities):
+        temps = [random.randint(1,20) for i in range(numy)]
+        f.write("\n")
+        f.write(",".join(map(str,temps)))
+    
+    f.close()
+
+for y in ["1","10","20","50"]:
+    f = open("teledata-"+y+"y-winter_day.csv","w")
+    
+    numy = 100
+    ystart = 1865
+    years = [ystart+i for i in range(numy)]
+    
+    numcities = 1218
+    
+    f.write(",".join(map(str,years)))
+    for i in range(numcities):
+        temps = [random.randint(1,200) for i in range(numy)]
+        f.write("\n")
+        f.write(",".join(map(str,temps)))
+    
+    f.close()
+
+for y in ["1","10","20","50"]:
+    f = open("teledata-"+y+"y-winter_extent.csv","w")
+    
+    numy = 100
+    ystart = 1865
+    years = [ystart+i for i in range(numy)]
+    
+    numcities = 1218
+    
+    f.write(",".join(map(str,years)))
+    for i in range(numcities):
+        temps = [random.randint(1,20) for i in range(numy)]
+        f.write("\n")
+        f.write(",".join(map(str,temps)))
+    
+    f.close()
