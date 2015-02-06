67711781220dd9ed65b4625c17175e38b5ddc04b
Andy Reagan <github@andyreagan.com>
2015-02-05 19:36:45 -0500

    margin for left axis

--- hedonometer/static/hedonometer/js/timeseries.js
+++ hedonometer/static/hedonometer/js/timeseries.js
@@ -214,7 +214,7 @@
 	top: 10,
 	right: 40,
 	bottom: 100,
-	left: 0
+	left: 15,
     },
     width = parseInt(d3.select("#bigbox").style("width"))-margin.left-margin.right,
     height = d3.max([300,parseInt(d3.select("#bigbox").style("width"))*0.5-margin.bottom-margin.top]),
