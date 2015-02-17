225b6b57d68d7c5a1cae42210bd96b008efebde5
Andy Reagan <github@andyreagan.com>
2015-02-17 15:54:27 -0500

    this should look up the username

--- twython_django/admin.py
+++ twython_django/admin.py
@@ -7,9 +7,13 @@ from twython_django.models import TwitterProfile,Annotation,MovieAnnotation
 
 class AnnotationAdmin(admin.ModelAdmin):
     search_fields = ('movie','user',)
-    list_display = ('movie','user','annotation','votes',)
+    list_display = ('movie','user','user_name','annotation','votes',)
     list_display_links = ('movie',)
     list_editable = ('votes',)
+
+    def user_name(self, obj):
+        return '%s'%(obj.User.oauth_token)
+    user_name.short_description = 'username'
 
 admin.site.register(TwitterProfile)
 admin.site.register(Annotation)
