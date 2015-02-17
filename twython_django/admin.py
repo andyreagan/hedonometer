d9264d9152273ca5eeea6d94bac0398f09b4c81a
Andy Reagan <github@andyreagan.com>
2015-02-17 16:07:33 -0500

    bigger script

--- twython_django/admin.py
+++ twython_django/admin.py
@@ -6,11 +6,12 @@ from django.contrib import admin
 from twython_django.models import TwitterProfile,Annotation,MovieAnnotation
 
 class AnnotationAdmin(admin.ModelAdmin):
-    search_fields = ('movie','user',)
-    list_display = ('movie','user','user_name','annotation','votes',)
+    search_fields = ('movie')
+    list_display = ('movie','user_name','annotation','votes',)
     list_display_links = ('movie',)
     list_editable = ('votes',)
 
+    # define a really fancy callback to get the username
     def user_name(self, obj):
         return '%s'%(obj.user.user.username)
     user_name.short_description = 'username'
