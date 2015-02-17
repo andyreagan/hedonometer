81de0199d89ee6de591a737411ff5939b51b718e
Andy Reagan <github@andyreagan.com>
2015-02-17 16:21:10 -0500

    bigger script

--- twython_django/admin.py
+++ twython_django/admin.py
@@ -5,19 +5,19 @@ from django.contrib import admin
 # Eventually we can create a database for the big events
 from twython_django.models import TwitterProfile,Annotation,MovieAnnotation
 
-class AnnotationAdmin(admin.ModelAdmin):
-    search_fields = ('movie')
-    list_display = ('movie','user_name','annotation','votes',)
-    list_display_links = ('movie',)
-    list_editable = ('votes',)
+# class AnnotationAdmin(admin.ModelAdmin):
+#     search_fields = ('movie')
+#     list_display = ('movie','user_name','annotation','votes',)
+#     list_display_links = ('movie',)
+#     list_editable = ('votes',)
 
-    # define a really fancy callback to get the username
-    def user_name(self, obj):
-        return '%s'%(obj.user.user.username)
-    user_name.short_description = 'username'
+#     # define a really fancy callback to get the username
+#     def user_name(self, obj):
+#         return '%s'%(obj.user.user.username)
+#     user_name.short_description = 'username'
 
 admin.site.register(TwitterProfile)
 admin.site.register(Annotation)
-# admin.site.register(MovieAnnotation)
-admin.site.register(MovieAnnotation,AnnotationAdmin)
+admin.site.register(MovieAnnotation)
+# admin.site.register(MovieAnnotation,AnnotationAdmin)
 
