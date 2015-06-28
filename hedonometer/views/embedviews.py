64c021eafb40b783802d341d1fa5ad731fd5897b
Andy Reagan <andy@andyreagan.com>
2015-06-27 22:52:11 -0400

    touching up the header

--- hedonometer/views/embedviews.py
+++ hedonometer/views/embedviews.py
@@ -16,15 +16,15 @@ def embedNYT(request,sectionref,sectioncomp):
     except:
         raise Http404
 
-    specialtext = '{0}\n{1}\nComparison happiness: avhapps\nWhat\'s making {2} updown than {3}:'.format("New York Times Wordshift",c.genre.title()+" section compared to "+r.genre.title()+" section",c.genre.title(),r.genre.title())
+    specialtext = '{0}\n{1}\nComparison happiness: avhapps\nWhat\'s making {2} updown than {3}:'.format('New York Times Wordshift',c.genre.title()+' section compared to '+r.genre.title()+' section',c.genre.title(),r.genre.title())
     if r.genre == 'all':
-        specialtext = '{0}\n{1}\nComparison happiness: avhapps\nWhat\'s making {2} updown than {3}:'.format("New York Times Wordshift",c.genre.title()+" section compared to the whole Times",c.genre.title(),"the whole Times")
+        specialtext = '{0}\n{1}\nComparison happiness: avhapps\nWhat\'s making {2} updown than {3}:'.format('New York Times Wordshift',c.genre.title()+' section compared to the whole Times',c.genre.title(),'the whole Times')
 
     filenames = {'h': 'dont matter',
                  'refFile': '/data/NYT/NYT_labVecs/%s.stripped.indexed' % r.filename,
-                 "refFileName": sectionref,
+                 'refFileName': sectionref,
                  'compFile': '/data/NYT/NYT_labVecs/%s.stripped.indexed' % c.filename,
-                 "compFileName": sectioncomp,
+                 'compFileName': sectioncomp,
                  'fulltext': specialtext,
                  'contextflag': 'main', # 'none'
                  'stopWords': '',
@@ -40,19 +40,19 @@ def embedCBS(request,hostref,hostcomp):
     # # but I do need a dates
     # logger.debug(some_hash)
 
-    refi = ["CHARLIEROSE","GAYLEKING","NORAHODONNELL","ALL"].index(hostref)
-    compi = ["CHARLIEROSE","GAYLEKING","NORAHODONNELL","ALL"].index(hostcomp)
-    hosts_nice = ["Charlie Rose","Gayle King","Norah O'Donnell","all"]
+    refi = ['CHARLIEROSE','GAYLEKING','NORAHODONNELL','ALL'].index(hostref)
+    compi = ['CHARLIEROSE','GAYLEKING','NORAHODONNELL','ALL'].index(hostcomp)
+    hosts_nice = ['Charlie Rose','Gayle King','Norah O\'Donnell','all']
     if refi == 3:
-        specialtext = '{0}\n{1}\nComparison happiness: avhapps\nWhat\'s making {2} updown than {3}:'.format("CBS Host Wordshift",hosts_nice[compi]+"'s lines compared to "+hosts_nice[refi]+" lines",hosts_nice[compi],hosts_nice[refi])
+        specialtext = '{0}\n{1}\nComparison happiness: avhapps\nWhat\'s making {2} updown than {3}:'.format('CBS Host Wordshift',hosts_nice[compi]+'\'s lines compared to '+hosts_nice[refi]+' lines',hosts_nice[compi],hosts_nice[refi])
     else:
-        specialtext = '{0}\n{1}\nComparison happiness: avhapps\nWhat\'s making {2} updown than {3}:'.format("CBS Host Wordshift",hosts_nice[compi]+"'s lines compared to "+hosts_nice[refi]+"'s lines",hosts_nice[compi],hosts_nice[refi])
+        specialtext = '{0}\n{1}\nComparison happiness: avhapps\nWhat\'s making {2} updown than {3}:'.format('CBS Host Wordshift',hosts_nice[compi]+'\'s lines compared to '+hosts_nice[refi]+'\'s lines',hosts_nice[compi],hosts_nice[refi])
 
     filenames = {'h': 'dont matter',
                  'refFile': 'http://hedonometer.org/data/CBS/%s.csv' % hostref,
-                 "refFileName": hostref,
+                 'refFileName': hostref,
                  'compFile': 'http://hedonometer.org/data/CBS/%s.csv' % hostcomp,
-                 "compFileName": hostcomp,
+                 'compFileName': hostcomp,
                  'fulltext': specialtext,
                  'contextflag': 'main', # 'none'
                  'stopWords': '',
@@ -68,7 +68,7 @@ def embedMainSimple(request,onedate):
     [year,month,day] = map(int,onedate.split('-'))
     d = datetime.datetime(year,month,day)
 
-    longer = d.strftime("%A, %B %e, %Y")
+    longer = d.strftime('%A, %B %e, %Y')
 
     event = Event.objects.filter(date=onedate)
     
@@ -96,8 +96,8 @@ def shifttest(request,reffile,compfile):
     # # but I do need a dates
     # logger.debug(some_hash)
 
-    filenames = {'refFile': "/static/hedonometer/"+reffile+".csv",
-                 'compFile': "/static/hedonometer/"+compfile+".csv",
+    filenames = {'refFile': '/static/hedonometer/'+reffile+'.csv',
+                 'compFile': '/static/hedonometer/'+compfile+'.csv',
     }
 
     # logger.debug(filenames)
@@ -114,15 +114,16 @@ def embedUpload(request,some_hash):
     # logger.debug(some_hash)
 
     # look up that hash in our database
-    # m = Embeddable.objects.filter(compFile__contains="2014-06-06")
+    # m = Embeddable.objects.filter(compFile__contains='2014-06-06')
     m = Embeddable.objects.get(h=some_hash)
 
     # grab the filenames of the data from the database
     # filenames = [m.refFile,m.compFile]
     filenames = {
         'refFile': m.refFile,
-        "compFile": m.compFile,
+        'compFile': m.compFile,
         'stopWords': m.stopWords,
+        'hash': some_hash,
     }
     if len(m.customTitleText) > 0:
         filenames['contextflag'] = 'justtitle'
@@ -142,9 +143,9 @@ def embedMain(request,dateref,datecomp):
 
     filenames = {'h': 'dont matter',
                  'refFile': 'http://hedonometer.org/data/word-vectors/%s.csv' % dateref,
-                 "refFileName": dateref,
+                 'refFileName': dateref,
                  'compFile': 'http://hedonometer.org/data/word-vectors/%s.csv' % datecomp,
-                 "compFileName": datecomp,
+                 'compFileName': datecomp,
                  'fulltext': '',
                  'contextflag': 'none',
                  'stopWords': '',
