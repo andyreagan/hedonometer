39c59fc53c77c8e27a33f0265af83e340a1ce85c
Andy <andy@andyreagan.com>
2022-03-11 17:25:24 +0000

    Add menu option split for ousio, fix js escaping of scores.

--- hedonometer/menu_context.py
+++ hedonometer/menu_context.py
@@ -3,10 +3,17 @@ import itertools
 from .models import Timeseries, WordList
 
 def include_timeseries_wordlist_menus(context):
-    timeseries_objects = Timeseries.objects.filter(showindropdown=True)
-    timeseries_grouped = {key: list(values) for key, values in itertools.groupby(Timeseries.objects.all(), lambda x: x.wordList.language_long())}
+    happs_timeseries_objects = Timeseries.objects.exclude(title__contains='ousio', showindropdown=False)
+    happs_timeseries_grouped = {key: list(values) for key, values in itertools.groupby(Timeseries.objects.exclude(title__contains='ousio'), lambda x: x.wordList.language_long())}
     wordlist_objects = WordList.objects.filter(showindropdown=True)
+    ousio_timeseries_objects = Timeseries.objects.filter(title__contains='ousio', showindropdown=True)
+    ousio_timeseries_grouped = {key: list(values) for key, values in itertools.groupby(Timeseries.objects.filter(title__contains='ousio'), lambda x: x.wordList.language_long())}
 
-    return {'timeseries_objects': timeseries_objects,
-            'timeseries_grouped': timeseries_grouped,
-            'wordlist_objects': wordlist_objects}
+
+    return {
+        'timeseries_objects': happs_timeseries_objects,
+        'timeseries_grouped': happs_timeseries_grouped,
+        'wordlist_objects': wordlist_objects,
+        'ousio_timeseries_objects': ousio_timeseries_objects,
+        'ousio_timeseries_grouped': ousio_timeseries_grouped,
+    }
